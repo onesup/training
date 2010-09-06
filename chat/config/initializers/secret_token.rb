b14356dd95c0f8d28cee574a806016633af4d87f
onesup <onesup.lee@gmail.com>
2010-09-06 17:40:24 +0900

    create simple chat room

---
+++ chat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = 'da868515efcb14159b32e8bd916993dc4399b2ae3ec109f50bdab4c62134eff6db63fb3f3608a0c464a2ca8fd12f1c96820dc174f3c47878350a914ea6cd59fc'
